478ee32fa6fcbc5ceadfbe474351047505145a0a
Jason Chen <purplvampire@gmail.com>
2023-08-08 22:33:50 +0800

    Update .gitignore to exclude files.

--- Sample/Section6-Backup/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from models.store import StoreModel
-from models.item import ItemModel
-
-# This is easy to import StoreModel and ItemModel from models package.
-# like: from models import StoreModel.
